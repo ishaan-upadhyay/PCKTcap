1be5e66c9fbe3778de3c12cab7a28ccfee8e3831
Ishaan <ishaanupadhyay1004@gmail.com>
2024-12-03 17:46:49 -0500

    ICMP display more information

--- pcktcap/app/components/layers/ICMP.tsx
+++ pcktcap/app/components/layers/ICMP.tsx
@@ -1,9 +1,35 @@
-
 import React, { useState } from 'react';
 
 const ICMP = ({ packet }: { packet: any }) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = () => setIsOpen(!isOpen);
+
+  const types: { [key: number]: string } = {
+    0: 'Echo Reply',
+    3: 'Destination Unreachable',
+    8: 'Echo Request',
+    11: 'Time Exceeded'
+  };
+
+  const codes: { [key: number]: { [key: number]: string } } = {
+    0: { 0: 'No Code' },
+    3: { 0: 'Net Unreachable', 
+         1: 'Host Unreachable', 
+         2: 'Protocol Unreachable', 
+         3: 'Port Unreachable', 
+         4: 'Fragmentation Needed and Don\'t Fragment was Set', 
+         5: 'Source Route Failed' 
+        },
+    8: { 0: 'No Code' },
+    11: { 0: 'Time to Live exceeded in Transit', 
+          1: 'Fragment Reassembly Time Exceeded' }
+  };
+
+  const modifiedPacket = {
+    ...packet,
+    type: `${packet.type} (${types[packet.type] || 'Unknown'})`,
+    code: `${packet.code} (${codes[packet.type]?.[packet.code] || 'Unknown'})`
+  };
 
   return (
     <div className="border p-4 rounded-lg shadow-md">
@@ -11,7 +37,7 @@ const ICMP = ({ packet }: { packet: any }) => {
         <strong>ICMP Layer</strong>
       </div>
       {isOpen && (
-        <pre className="mt-2 p-2 rounded">{JSON.stringify(packet, null, 2)}</pre>
+        <pre className="mt-2 p-2 rounded">{JSON.stringify(modifiedPacket, null, 2)}</pre>
       )}
     </div>
   );
